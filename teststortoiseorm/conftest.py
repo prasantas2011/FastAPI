21f5dc49cccf41606469cd240aa172e2c93dbfe1
prasantas2011 <prasantakus@gmail.com>
2025-09-03 23:58:13 +0530

    final test code with coverage

--- teststortoiseorm/conftest.py
+++ teststortoiseorm/conftest.py
@@ -3,20 +3,30 @@ from tortoise import Tortoise
 from httpx import AsyncClient, ASGITransport
 import modelstortoise
 from mainwithtortoise import app
+import os
 
-TEST_DB_URL = "sqlite://:memory:"  # in-memory DB for tests
+TEST_DB_FILE = "test3.db"
+TEST_DB_URL = f"sqlite://./{TEST_DB_FILE}"
 
-# Use async fixture for Tortoise initialization
 @pytest_asyncio.fixture(scope="session", autouse=True)
 async def init_tortoise():
+    # Remove old DB file if exists
+    if os.path.exists(TEST_DB_FILE):
+        os.remove(TEST_DB_FILE)
+
+    # Init new DB
     await Tortoise.init(
         db_url=TEST_DB_URL,
-        modules={"models": ["modelstortoise"]},  # your models
-        _create_db=True  # auto-create DB if needed
+        modules={"models": ["modelstortoise"]},
+        _create_db=True
     )
     await Tortoise.generate_schemas()
     yield
     await Tortoise.close_connections()
+
+     # Delete DB file after all tests
+    if os.path.exists(TEST_DB_FILE):
+        os.remove(TEST_DB_FILE)
 
 # Async test client fixture
 @pytest_asyncio.fixture
